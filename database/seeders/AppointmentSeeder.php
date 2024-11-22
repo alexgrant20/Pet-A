5fa7e07c1c611c4ee44f54967c35c80331d7ad0d
Lorencia <loreenncia@gmail.com>
2024-11-22 10:30:42 +0700

    appointment seeder

--- database/seeders/AppointmentSeeder.php
+++ database/seeders/AppointmentSeeder.php
@@ -21,17 +21,18 @@ class AppointmentSeeder extends Seeder
          'service_type_id' => 1,
          'veterinarian_id' => 1,
          'appointment_note' => 'Anjing saya muntah-muntah',
-         'appointment_date' => '2024-09-27',
+         'appointment_date' => '2024-11-26',
       ]);
 
       Appointment::create([
          'pet_owner_id' => 1,
-         'pet_id' => 1,
-         'appointment_schedule_id' => 4,
+         'pet_id' => 2,
+         'appointment_schedule_id' => 82,
          'clinic_id' => 1,
          'service_type_id' => 1,
-         'veterinarian_id' => 2,
-         'appointment_date' => '2024-10-22',
+         'veterinarian_id' => 1,
+         'appointment_note' => 'Anjing saya kejang',
+         'appointment_date' => '2024-11-28',
       ]);
    }
 }
