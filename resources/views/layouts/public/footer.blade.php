00a577b869e79547bbaaaed9fbd4f8a177ccfc8c
Steven Alexander Grant <alexgrant200@yahoo.com>
2024-11-23 20:05:44 +0700

    add responve desgin

--- resources/views/layouts/public/footer.blade.php
+++ resources/views/layouts/public/footer.blade.php
@@ -1,9 +1,9 @@
-<footer class="items-center justify-between flex py-4 pe-6 text-neutral-50 text-opacity-70 bottom-0 w-full">
-  <div class="flex items-center gap-3 justify-end">
+<footer class="items-center justify-center md:justify-between flex py-4 pe-6 text-neutral-50 text-opacity-70 bottom-0 w-full">
+  <div class="md:flex items-center gap-3 justify-end hidden ms-3">
    <i class="fa-solid fa-paw"></i>{{ $totalPetSaved }} Pets Saved
   </div>
   <aside>
-   <p>
+   <p class="hidden sm:block">
      &copy; 2024 Pet-A - All right reserved
    </p>
  </aside>
